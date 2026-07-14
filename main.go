5d35ccd40ebd45dc508478239cd7cc16b2abfa14
gustavo-silva98 <silva.gustavo98@gmail.com>
2026-07-14 16:10:52 -0300

    Tradução pt-br pra inglês e comentários de step do código

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,13 +22,13 @@ var (
 func main() {
 	config, err := loadConfigParameters()
 	if err != nil {
-		log.Printf("Falha ao carregar parametros config: %v", err)
+		log.Printf("Failed to load Config Parameters: %v", err)
 		return
 	}
 	ctx := context.Background()
 	provider, err := oidc.NewProvider(ctx, config.issuerURL)
 	if err != nil {
-		log.Printf("Falha ao inicializar OIDC provider: %v", err)
+		log.Printf("Failed to initialize OIDC provider: %v", err)
 		return
 	}
 
@@ -49,29 +50,30 @@ func main() {
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `<html><body><a href="/login"> Clique para logar! </a></body></html>`)
+	fmt.Fprintf(w, `<html><body><a href="/login"> Click here to logon! </a></body></html>`)
 }
 
 func handleLoginOk(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `<html><body> Login Bem Sucedido! </body></html>`)
+	fmt.Fprintf(w, `<html><body> Login Successful! </body></html>`)
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
 	state, err := createRandomString(32)
 	if err != nil {
-		log.Printf("Falha na criação de State: %v", err)
+		log.Printf("Failed to generate State: %v", err)
 		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
 		return
 	}
 	nonce, err := createRandomString(32)
 	if err != nil {
-		log.Printf("Falha na criação de State: %v", err)
+		log.Printf("Failed to generate Nonce: %v", err)
 		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
 		return
 	}
-	code_verifier := oauth2.GenerateVerifier() // Criação de codigo PKCE
+	codeVerifier := oauth2.GenerateVerifier() // Generate PKCE Code
+	log.Println("STEP 1: State, Nonce and PKCE code generated.")
 
-	// Parametro Secure ausente pois se trata de um tutorial. Habilitar em produção com TLS.
+	// Secure Parameter is missing because this is a tutorial. In production, use Secure/TLS when setting a cookie
 	http.SetCookie(w, &http.Cookie{
 		Name:     "state",
 		Value:    state,
@@ -89,104 +91,110 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 		Expires:  time.Now().Add(10 * time.Minute),
 	})
 	http.SetCookie(w, &http.Cookie{
-		Name:     "code_verifier",
-		Value:    code_verifier,
+		Name:     "codeVerifier",
+		Value:    codeVerifier,
 		Path:     "/",
 		HttpOnly: true,
 		SameSite: http.SameSiteLaxMode,
 		Expires:  time.Now().Add(10 * time.Minute),
 	})
-
-	http.Redirect(w, r, oauth2Config.AuthCodeURL(state, oidc.Nonce(nonce), oauth2.S256ChallengeOption(code_verifier)), http.StatusFound)
+	redirectUrl := oauth2Config.AuthCodeURL(state, oidc.Nonce(nonce), oauth2.S256ChallengeOption(codeVerifier))
+	log.Println("STEP 1: Nonce, State and PKCE are set as a cookie in the browser.")
+	log.Println("STEP 2: User will be redirected to MS Auth page")
+	log.Printf("Redirect URL: %v", redirectUrl)
+	http.Redirect(w, r, redirectUrl, http.StatusFound)
 }
 
 func handleCallback(w http.ResponseWriter, r *http.Request) {
 	state, err := r.Cookie("state")
 	if err != nil {
-		log.Printf("Falha ao achar o cookie state")
+		log.Printf("Failed to extract state value from cookie")
 		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
 		return
 	}
-	if r.URL.Query().Get("state") != state.Value {
-		log.Printf("Falha na validação de State. Val. Esperado: %v - Val. recebido: %v", state, r.URL.Query().Get("state"))
-		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
-		return
-	}
-	code_verifier, err := r.Cookie("code_verifier")
-	if err != nil {
-		log.Printf("Falha ao achar o cookie verifier")
-		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
-		return
-	}
-	ctx := r.Context()
-	token, err := oauth2Config.Exchange(ctx, r.URL.Query().Get("code"), oauth2.VerifierOption(code_verifier.Value))
-	if err != nil {
-		log.Printf("Falha na troca de token Exchange: %v", err)
-		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
-		return
-	}
-	rawIDToken, ok := token.Extra("id_token").(string)
-	if !ok {
-		log.Print("Sem id_token na resposta")
-		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
-		return
-	}
-	idtoken, err := verifier.Verify(ctx, rawIDToken)
-	if err != nil {
-		log.Printf("Falha na verificação do ID Token: %v", err)
-		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
-		return
-	}
-
-	var claims struct {
-		Email     string   `json:"email"`
-		Name      string   `json:"name"`
-		Sub       string   `json:"sub"`
-		Tid       string   `json:"tid"`
-		Oid       string   `json:"oid"`
-		Aud       string   `json:"aud"`
-		Idp       string   `json:"idp"`
-		Iat       int      `json:"iat"`
-		Exp       int      `json:"exp"`
-		Nonce     string   `json:"nonce"`
-		UPN       string   `json:"upn"`
-		Sid       string   `json:"sid"` //Session ID
-		HasGroups bool     `json:"hasgroups"`
-		Groups    []string `json:"groups"`
-	}
-
-	if err := idtoken.Claims(&claims); err != nil {
-		log.Printf("Erro ao parsear claims: %v", err)
-		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
-		return
-	}
-
 	nonce, err := r.Cookie("nonce")
 	if err != nil {
 		log.Printf("Falha ao achar o cookie nonce")
 		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
 		return
 	}
+	code_verifier, err := r.Cookie("codeVerifier")
+	if err != nil {
+		log.Printf("Failed to extract PKCE code value from cookie")
+		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
+		return
+	}
+	log.Println("STEP 3: Cookie values from state, nonce and PKCE has been succeed!")
 
-	if nonce.Value != claims.Nonce {
-		log.Printf("Falha ao validar Nonce. Val. Esperado: %v - Val. Recebido: %v", nonce.Value, claims.Nonce)
+	if r.URL.Query().Get("state") != state.Value {
+		log.Printf("Failed to validate State Value. Val. Exp: %v - Val. Received: %v", state, r.URL.Query().Get("state"))
+		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
+		return
+	}
+	log.Println("STEP 3: State validation has been succeed!")
+
+	ctx := r.Context()
+	token, err := oauth2Config.Exchange(ctx, r.URL.Query().Get("code"), oauth2.VerifierOption(code_verifier.Value))
+	if err != nil {
+		log.Printf("Failed to perform token Exchange: %v", err)
+		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
+		return
+	}
+	log.Println("STEP 4: Exchange code for token has been succeed!")
+	log.Printf("STEP 4: AccessToken Lenght: %v", len(token.AccessToken))
+
+	rawIDToken, ok := token.Extra("id_token").(string)
+	if !ok {
+		log.Print("Id_token is missing in token received")
 		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
 		return
 	}
 
-	//response, _ := json.MarshalIndent(claims, "", " ")
-	//w.Header().Set("Content-Type", "application/json")
-	//w.Write(response)
+	idtoken, err := verifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		log.Printf("Fail on Id_Token verification: %v", err)
+		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
+		return
+	}
+	log.Println("STEP 5: ID_Token verification by oidc package (signature, issuer, aud) has been succeed!")
 
-	// Invalidando cookie de state e nonce
+	var claims struct {
+		Email  string   `json:"email"`
+		Name   string   `json:"name"`
+		Sub    string   `json:"sub"`
+		Tid    string   `json:"tid"`
+		Oid    string   `json:"oid"`
+		Aud    string   `json:"aud"`
+		Nonce  string   `json:"nonce"`
+		Groups []string `json:"groups"` // Claim that you can use to build authorization based on security groups
+	}
+
+	if err := idtoken.Claims(&claims); err != nil {
+		log.Printf("Error parsing id_token claims: %v", err)
+		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
+		return
+	}
+
+	if nonce.Value != claims.Nonce {
+		log.Printf("Failed to validate Nonce. Val. Exp: %v - Val. Received: %v", nonce.Value, claims.Nonce)
+		http.Error(w, "Falha na autenticação", http.StatusInternalServerError)
+		return
+	}
+	log.Println("STEP 5: Nonce value has been succeed!")
+
+	response, _ := json.MarshalIndent(claims, "", " ")
+	//w.Header().Set("Content-Type", "application/json")
+	log.Println("STEP 5: ID_TOKEN claims below. Use this to build your session management")
+	log.Println(string(response))
+
+	// Cookie invalidations
 	http.SetCookie(w, &http.Cookie{
 		Name:     "state",
 		Value:    "",
 		Path:     "/",
 		HttpOnly: true,
 		SameSite: http.SameSiteLaxMode,
-		Expires:  time.Now().Add(10 * time.Minute),
-		MaxAge:   -1, // Parametro <0 significa deleção de cookie
+		MaxAge:   -1, // MaxAge <0 means that the cookie will be deleted
 	})
 	http.SetCookie(w, &http.Cookie{
 		Name:     "nonce",
@@ -194,19 +202,18 @@ func handleCallback(w http.ResponseWriter, r *http.Request) {
 		Path:     "/",
 		HttpOnly: true,
 		SameSite: http.SameSiteLaxMode,
-		Expires:  time.Now().Add(10 * time.Minute),
-		MaxAge:   -1, // Parametro <0 significa deleção de cookie
+		MaxAge:   -1, // MaxAge <0 means that the cookie will be deleted
 	})
 	http.SetCookie(w, &http.Cookie{
-		Name:     "code_verifier",
+		Name:     "codeVerifier",
 		Value:    "",
 		Path:     "/",
 		HttpOnly: true,
 		SameSite: http.SameSiteLaxMode,
-		Expires:  time.Now().Add(10 * time.Minute),
-		MaxAge:   -1, // Parametro <0 significa deleção de cookie
+		MaxAge:   -1, // MaxAge <0 means that the cookie will be deleted
 	})
-
+	log.Println("STEP 5: Cookies PKCE code, nonce and state deleted from the browser.")
+	log.Println("STEP 5: Redirecting user to happy path. /login-ok route.")
 	http.Redirect(w, r, "/login-ok", http.StatusFound)
 
 }
